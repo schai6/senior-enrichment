c95850053cf215db6a9e42be02fcafbb6bf50690
Sam Chai <sam.chai0501@gmail.com>
2017-12-05 14:11:01 -0500

    add associations and export db/models

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,10 +3,17 @@
 const db = require('../index');
 
 // Require all the models
-	// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
-	// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
-	// Exporting all models from here seems like a good idea!
-
+// Running each model (i.e. table) module (i.e. file) registers each model into our sequelize db
+// This works if we all use the same Sequelize instance (instantiated in and exported from `/db/index.js`)
+// Exporting all models from here seems like a good idea!
+const Campus = require('./Campus');
+const Student = require('./Student');
 // This is also probably a good place for you to set up your associations
+Student.hasOne(Campus);
+Campus.hasMany(Student);
 
-module.exports = db
+module.exports = {
+  db,
+  Campus,
+  Student
+};
