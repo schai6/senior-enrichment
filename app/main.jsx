db97f6568b4cf970d1d286be481e6809298fcb8a
Sam Chai <sam.chai0501@gmail.com>
2017-12-05 22:49:29 -0500

    add router

--- app/main.jsx
+++ app/main.jsx
@@ -1,14 +1,16 @@
-'use strict'
-import React from 'react'
-import {render} from 'react-dom'
-import { Provider } from 'react-redux'
+'use strict';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowseRouter as Router } from 'react-router-dom';
+import store from './store';
+import Root from './components/Root';
 
-import store from './store'
-import Root from './components/Root'
-
-render (
+render(
   <Provider store={store}>
-    <Root/>
+    <Router>
+      <Root />
+    </Router>
   </Provider>,
   document.getElementById('main')
-)
+);
