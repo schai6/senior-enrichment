a48c4db42802c03613e807eae335769944a1cbe2
Sam Chai <sam.chai0501@gmail.com>
2017-12-05 22:58:19 -0500

    fix typo

--- app/main.jsx
+++ app/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowseRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import Root from './components/Root';
 
