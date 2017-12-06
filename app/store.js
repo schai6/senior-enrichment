d0dd36470ac22067cf64bab3a69da592b77f7cca
Sam Chai <sam.chai0501@gmail.com>
2017-12-06 12:27:27 -0500

    fix paths to reducers

--- app/store.js
+++ app/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
-import reducers from './store/';
+import reducers from './reducers';
 import loggingMiddleware from 'redux-logger'; // https://github.com/evgenyrodionov/redux-logger
 import thunkMiddleware from 'redux-thunk'; // https://github.com/gaearon/redux-thunk
 
 export default createStore(reducers, applyMiddleware(thunkMiddleware, loggingMiddleware));
 
-export * from './store/students';
+export * from './reducers/students';
