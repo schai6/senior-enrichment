f6b8a38dfc842cfff9f5f9d9d8376b22622f3324
Sam Chai <sam.chai0501@gmail.com>
2017-12-06 10:15:58 -0500

    add campus routes

---
+++ server/api/campuses.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Campus } = require('../db/models');
+
+
+router.get('/', (req, res, next) => {
+  Campus.findAll()
+  .then(campuses => {
+    res.json(campuses);
+  })
+  .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  Campus.findById(req.params.id)
+  .then(Campus => {
+    res.json(Campus);
+  })
+  .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+  Campus.create(req.body)
+  .then(Campus => {
+    res.json(Campus);
+  })
+  .catch(next);
+});
+
+router.put('/:id', (req, res, next) => {
+  Campus.findById(req.params.id)
+  .then(Campus => {
+    return Campus.update(req.body);
+  })
+  .then(Campus => {
+    res.json(Campus);
+  })
+  .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  Campus.destroy({
+    where: {
+      id: +req.params.id
+    }
+  })
+  .then(res.sendStatus(202))
+  .catch(next);
+});
+
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,14 @@
-'use strict'
-const router = require('express').Router()
-const db = require('../db')
-
+'use strict';
+const router = require('express').Router();
 
 // If you aren't getting to this object, but rather the index.html (something with a joke) your path is wrong.
-	// I know this because we automatically send index.html for all requests that don't make sense in our backend.
-	// Ideally you would have something to handle this, so if you have time try that out!
-router.get('/hello', (req, res) => res.send({hello: 'world'}));
+// I know this because we automatically send index.html for all requests that don't make sense in our backend.
+// Ideally you would have something to handle this, so if you have time try that out!
+router.get('/hello', (req, res) => res.send({
+  hello: 'world'
+}));
 router.use('/students', require('./students'));
+router.use('/campuses', require('./campuses'));
 
 // You can put all routes in this file; HOWEVER, this file should almost be like a table of contents for the routers you create
 
