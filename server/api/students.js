66a0be4a141ebda08f178c708b70085de9d14122
Sam Chai <sam.chai0501@gmail.com>
2017-12-05 23:55:03 -0500

    add students get,post,put, and delete routes

---
+++ server/api/students.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { Student, Campus } = require('../db/models');
+
+router.get('/', (req, res, next) => {
+  Student.findAll()
+  .then(students => {
+    res.json(students);
+  })
+  .catch(next);
+});
+
+router.get('/:id', (req, res, next) => {
+  Student.findById(req.params.id)
+  .then(student => {
+    res.json(student);
+  })
+  .catch(next);
+});
+
+router.post('/', (req, res, next) => {
+  Student.create(req.body)
+  .then(student => {
+    res.json(student);
+  })
+  .catch(next);
+});
+
+router.put('/:id', (req, res, next) => {
+  Student.findById(req.params.id)
+  .then(student => {
+    return student.update(req.body);
+  })
+  .then(student => {
+    res.json(student);
+  })
+  .catch(next);
+});
+
+router.delete('/:id', (req, res, next) => {
+  Student.destroy({
+    where: {
+      id: +req.params.id
+    }
+  })
+  .then(res.sendStatus(202))
+  .catch(next);
+});
+
+module.exports = router;
