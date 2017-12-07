c2dd67125f59bc20f08842606d45f50b6bfe342e
Sam Chai <sam.chai0501@gmail.com>
2017-12-07 10:18:28 -0500

    add a user button to view the student

--- app/components/AllStudents.jsx
+++ app/components/AllStudents.jsx
@@ -21,15 +21,16 @@ class AllStudents extends React.Component {
   render() {
     return (
       <div>
-        <Modal size='tiny' open={this.state.modalOpen} onClose={this.handleClose.bind(this)} trigger={<Button onClick={this.handleOpen.bind(this)}>ADD STUDENT</Button>}>
+        <Modal size='tiny' open={this.state.modalOpen} onClose={this.handleClose.bind(this)} trigger={<Button onClick={this.handleOpen.bind(this)} className='add-student-button'>ADD STUDENT</Button>}>
           <Modal.Header>Add a Student</Modal.Header>
           <Modal.Content>
             <StudentForm handleClose={this.handleClose.bind(this)} handleFormSubmit={this.props.handleFormSubmit} />
           </Modal.Content>
         </Modal>
-        <Table textAlign='center' celled selectable collapsing compact size='large'>
+        <Table textAlign='center' definition celled selectable size='large'>
           <Table.Header>
             <Table.Row>
+              <Table.HeaderCell width={1}/>
               <Table.HeaderCell width={1}>#</Table.HeaderCell>
               <Table.HeaderCell>Name</Table.HeaderCell>
               <Table.HeaderCell>Campus</Table.HeaderCell>
@@ -40,7 +41,8 @@ class AllStudents extends React.Component {
             {this.props.students.map((student, i) => {
               return (
                 <Table.Row key={student.id}>
-                  <Table.Cell onClick={()=>console.log('hihihi')}>{i + 1}</Table.Cell>
+                  <Table.Cell textAlign='center'><Button icon onClick={() => console.log('hihihih')}><Icon name='user' size='large' /></Button></Table.Cell>
+                  <Table.Cell onClick={() => console.log('hihihi')}>{i + 1}</Table.Cell>
                   <Table.Cell>{student.name}</Table.Cell>
                   <Table.Cell>{student.campus}</Table.Cell>
                   <Table.Cell textAlign='center'><Button icon onClick={() => this.props.handleClick(student.id)}><Icon name='user delete' size='large' /></Button></Table.Cell>
