48678bff3a5a8856ef22226adb1eaa6557354c54
Sam Chai <sam.chai0501@gmail.com>
2017-12-06 23:45:07 -0500

    close form on submit

--- app/components/AllStudents.jsx
+++ app/components/AllStudents.jsx
@@ -1,36 +1,58 @@
 import React from 'react';
-import { Table, Icon, Button } from 'semantic-ui-react';
+import { Table, Icon, Button, Modal } from 'semantic-ui-react';
 import StudentForm from './StudentForm';
 
-const AllStudents = (props) => {
-  return (
-    <div>
-      <StudentForm handleFormSubmit={props.handleFormSubmit} />
-      <Table celled selectable sortable>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell width={1}>#</Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Campus</Table.HeaderCell>
-            <Table.HeaderCell width={1}>Remove</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body >
-          {props.students.map((student, i) => {
-            return (
-              <Table.Row key={student.id}>
-                <Table.Cell>{i + 1}</Table.Cell>
-                <Table.Cell>{student.name}</Table.Cell>
-                <Table.Cell>{student.campus}</Table.Cell>
-                <Table.Cell textAlign='center'><Button icon onClick={() => props.handleClick(student.id)}><Icon name='user delete' size='large' /></Button></Table.Cell>
-              </Table.Row>
-            );
-          }
-          )}
-        </Table.Body>
-      </Table>
-    </div >
-  );
+class AllStudents extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
+  handleOpen() {
+    this.setState({ modalOpen: true });
+  }
+
+  handleClose() {
+    this.setState({ modalOpen: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <Modal size='tiny' open={this.state.modalOpen} onClose={this.handleClose.bind(this)} trigger={<Button onClick={this.handleOpen.bind(this)}>ADD STUDENT</Button>}>
+          <Modal.Header>Add a Student</Modal.Header>
+          <Modal.Content>
+            <StudentForm handleClose={this.handleClose.bind(this)} handleFormSubmit={this.props.handleFormSubmit} />
+          </Modal.Content>
+        </Modal>
+        <Table celled selectable sortable>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell width={1}>#</Table.HeaderCell>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Campus</Table.HeaderCell>
+              <Table.HeaderCell width={1}>Remove</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body >
+            {this.props.students.map((student, i) => {
+              return (
+                <Table.Row key={student.id}>
+                  <Table.Cell>{i + 1}</Table.Cell>
+                  <Table.Cell>{student.name}</Table.Cell>
+                  <Table.Cell>{student.campus}</Table.Cell>
+                  <Table.Cell textAlign='center'><Button icon onClick={() => this.props.handleClick(student.id)}><Icon name='user delete' size='large' /></Button></Table.Cell>
+                </Table.Row>
+              );
+            }
+            )}
+          </Table.Body>
+        </Table>
+      </div >
+    );
+  }
 };
 
 export default AllStudents;
--- app/components/StudentForm.jsx
+++ app/components/StudentForm.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Form, Button, Modal } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 import { InputField } from 'react-semantic-redux-form';
 
 const StudentForm = (props) => {
   return (
-    <Modal size='tiny' trigger={<Button>ADD STUDENT</Button>}>
-      <Modal.Header>Add a Student</Modal.Header>
-      <Modal.Content>
         <Form onSubmit={props.handleSubmit((values) => props.handleFormSubmit(values))}>
           <Form.Group>
             <Field name='firstName' component={InputField} label='First Name' placeholder='First Name' />
@@ -15,10 +12,8 @@ const StudentForm = (props) => {
           </Form.Group>
           <Field name='email' component={InputField} label='Email' type='email' placeholder='Email' />
           <Field name='campus' component={InputField} label='Campus' placeholder='Campus' />
-          <Button type='submit'>Submit</Button>
-        </Form></Modal.Content>
-    </Modal>
-
+          <Button type='submit' onClick={props.handleClose}>Submit</Button>
+        </Form>
   );
 };
 
