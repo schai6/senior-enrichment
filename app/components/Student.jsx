ebc2911db1ae4a266d2e3aaec498184617543961
Sam Chai <sam.chai0501@gmail.com>
2017-12-06 19:44:12 -0500

    fix table mapping and add modal for add student

--- app/components/Student.jsx
+++ app/components/Student.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Table, Icon, Button } from 'semantic-ui-react';
+import { Table, Icon, Button, Modal } from 'semantic-ui-react';
+import StudentForm from './StudentForm';
 
 const Student = (props) => {
   return (
     <div>
+      <Modal size='tiny' trigger={<Button>ADD STUDENT</Button>}>
+        <Modal.Header>Add a Student</Modal.Header>
+        <Modal.Content><StudentForm /></Modal.Content>
+      </Modal>
       <Table celled selectable sortable>
         <Table.Header>
           <Table.Row>
@@ -13,21 +18,21 @@ const Student = (props) => {
             <Table.HeaderCell width={1}>Remove</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        {props.students.map((student, i) => {
-          return (
-            <Table.Body key={student.id}>
-              <Table.Row>
+        <Table.Body >
+          {props.students.map((student, i) => {
+            return (
+              <Table.Row key={student.id}>
                 <Table.Cell>{i + 1}</Table.Cell>
                 <Table.Cell>{student.name}</Table.Cell>
                 <Table.Cell>{student.campus}</Table.Cell>
                 <Table.Cell textAlign='center'><Button icon onClick={() => props.handleClick(student.id)}><Icon name='user delete' size='large' /></Button></Table.Cell>
               </Table.Row>
-            </Table.Body>
-          );
-        }
-        )}
+            );
+          }
+          )}
+        </Table.Body>
       </Table>
-    </div>
+    </div >
   );
 };
 
