c10c973f6b1694ea52a28ac57d4ecdc6dff3c791
Sam Chai <sam.chai0501@gmail.com>
2017-12-06 21:25:20 -0500

    add campus reducer

---
+++ app/reducers/campuses.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+export const FETCH_CAMPUSES = 'FETCH_CAMPUSES';
+export const REMOVE_CAMPUS = 'REMOVE_CAMPUS';
+export const GET_CAMPUSES = 'GET_CAMPUSES';
+export const GET_CAMPUS = 'GET_CAMPUS';
+
+export const fetchCampuses = () => {
+  return dispatch => {
+    return axios.get('/api/campuses')
+      .then(campuses => {
+        dispatch(getCampuses(campuses.data));
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+};
+
+export const removeCampus = (campusId) => {
+  return dispatch => {
+    return axios.delete('/api/campuses/' + campusId)
+      .then(() => {
+        dispatch(fetchCampuses());
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+};
+
+export const postCampus = (campus) => {
+  return dispatch => {
+    return axios.post('/api/campuses/', campus)
+    .then(res => res.data)
+    .then(campus => dispatch(getCampuses(campus)));
+  };
+};
+
+export const getCampuses = (campus) => {
+  return {
+    type: GET_CAMPUSES,
+    campus
+  };
+};
+
+export const getCampus = (campus) => {
+  return {
+    type: GET_CAMPUS,
+    campus
+  };
+};
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case GET_CAMPUSES:
+      return action.campuses;
+    case GET_CAMPUS:
+      return [...state, action.campus];
+    default:
+      return state;
+  }
+};
