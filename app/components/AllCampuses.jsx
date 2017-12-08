023e36d90fe7898b1e7ba2aa992baca70d6ae139
Sam Chai <sam.chai0501@gmail.com>
2017-12-08 01:11:41 -0500

    populate campuses using database

--- app/components/AllCampuses.jsx
+++ app/components/AllCampuses.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
 
 const AllCampuses = (props) => {
+  const colors = ['red', 'orange', 'yellow', 'olive', 'green', 'teal', 'blue', 'violet', 'purple', 'pink', 'brown', 'grey', 'black'];
+  const campuses = props.campuses;
+  let rows = [];
+  for (let i = 0; i < campuses.length; i += 4) {
+    rows.push([]);
+    for (let j = i; j < i + 4; j++) {
+      rows[i/4].push(campuses[j]);
+    }
+  }
   return (
-    <div>
-      <Grid padded>
-        <Grid.Row columns="4">
-          <Grid.Column>
-            <Image label={{ as: 'a', color: 'black', content: 'Earth', ribbon: true }} src='/planets/earth.jpg' />
-          </Grid.Column>
-          <Grid.Column>
-            <Image label={{ as: 'a', color: 'black', content: 'Jupiter', ribbon: true }}src='/planets/jupiter.jpg' />
-          </Grid.Column>
-          <Grid.Column>
-            <Image label={{ as: 'a', color: 'black', content: 'Mars', ribbon: true }}src='/planets/mars.jpg' />
-          </Grid.Column>
-          <Grid.Column>
-            <Image label={{ as: 'a', color: 'black', content: 'Mercury', ribbon: true }}src='/planets/mercury.jpg' />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div >
+    <Grid padded>
+      {rows.map((row, i) => {
+        return (<Grid.Row key={i} columns="4">
+          {row.map(campus => {
+            return (<Grid.Column key={campus.id}>
+              <Image label={{ color: colors[Math.floor(Math.random() * colors.length)], content: campus.name, ribbon: true }} src={campus.imageUrl} />
+            </Grid.Column>);
+          })}
+        </Grid.Row>);
+      })
+      }
+    </Grid>
   );
 };
 
