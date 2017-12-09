6ebfa7da7897e06fcbcdca6d41ab92a4afd19357
Sam Chai <sam.chai0501@gmail.com>
2017-12-09 01:10:51 -0500

    installed circular-dependencies checker and material-ui

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 'use strict';
 
 const { resolve } = require('path')
+let CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
   entry: './app/main',
+  plugins: [
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      failOnError: true,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    })
+  ],
   output: {
     path: __dirname,
     filename: './public/bundle.js'
