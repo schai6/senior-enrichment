39f6161b967ad60ea2a82daade766f9f1b67afe0
Sam Chai <sam.chai0501@gmail.com>
2017-12-09 01:13:00 -0500

    add routing for urls

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import store, { fetchStudents, fetchCampuses } from '../store';
 import NavBar from './NavBar';
-import { Redirect, Switch } from 'react-router-dom';
+import { Redirect, Switch, Route } from 'react-router-dom';
+
 export default class Root extends React.Component {
   componentDidMount() {
     store.dispatch(fetchStudents());
@@ -11,8 +12,11 @@ export default class Root extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Switch >
-          <NavBar />
+          <Route path="/home" />
+          <Route path="/students" />
+          <Route path="/campuses" />
           <Redirect to="/home" />
         </Switch >
       </div>
