27e49d9b956ede07251401720e9629cf01804ed8
Sam Chai <sam.chai0501@gmail.com>
2017-12-09 16:08:09 -0500

    move component rendering from navbar to root

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
-import AllStudentsContainer from '../containers/AllStudentsContainer';
-import AllCampusesContainer from '../containers/AllCampusesContainer';
+import { Menu } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 import { withRouter } from 'react-router';
 
@@ -9,14 +7,6 @@ const NavBar = (props) => {
   //slash so the tab stays selected when viewing single campus/student
   const slash = props.location.pathname.indexOf('/', 1);
   const tab = slash > -1 ? props.location.pathname.slice(1, slash) : props.location.pathname.slice(1);
-  let currentPage;
-  if (tab === 'campuses') {
-    currentPage = <AllCampusesContainer />;
-  } else if (tab === 'students') {
-    currentPage = <AllStudentsContainer />;
-  } else {
-    currentPage = 'blah';
-  }
   return (
     <div>
       <Menu pointing secondary>
@@ -26,9 +16,6 @@ const NavBar = (props) => {
           <NavLink to={`/students`}><Menu.Item name='Students' active={tab === 'students'} /></NavLink>
         </Menu.Menu>
       </Menu>
-      <Segment attached='bottom' raised padded>
-        {currentPage}
-      </Segment>
     </div>
   );
 };
--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import store, { fetchStudents, fetchCampuses } from '../store';
 import NavBar from './NavBar';
 import { Redirect, Switch, Route } from 'react-router-dom';
-
+import { Segment } from 'semantic-ui-react';
+import AllCampusesContainer from '../containers/AllCampusesContainer';
+import AllStudentsContainer from '../containers/AllStudentsContainer';
 export default class Root extends React.Component {
   componentDidMount() {
     store.dispatch(fetchStudents());
@@ -13,14 +15,16 @@ export default class Root extends React.Component {
     return (
       <div>
         <NavBar />
-        <Switch >
-          <Route path="/home/" />
-          <Route exact path="/students/" />
-          <Route exact path="/campuses/" />
-          <Route path="/students/:id/" />
-          <Route path="/campuses/:id/" />
-          <Redirect to="/home/" />
-        </Switch >
+        <Segment attached='bottom' raised padded>
+          <Switch >
+            <Route path="/home/" />
+            <Route exact path="/students/" component={AllStudentsContainer} />
+            <Route exact path="/campuses/" component={AllCampusesContainer} />
+            <Route path="/students/:id/" />
+            <Route path="/campuses/:id/" />
+            <Redirect to="/home/" />
+          </Switch >
+        </Segment>
       </div>
     );
   }
