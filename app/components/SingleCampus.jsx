3384d557c8425bf91fae62754498a637d6fdc171
Sam Chai <sam.chai0501@gmail.com>
2017-12-09 23:58:07 -0500

    add single campus component that uses allstudents component

---
+++ app/components/SingleCampus.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Header, Image } from 'semantic-ui-react';
+import AllStudentsContainer from '../containers/AllStudentsContainer';
+
+const SingleCampus = (props) => {
+  const campus = props.campus;
+  return (
+    <div>
+      <Header as='h2' icon>
+        <Image src={campus.imageUrl} />
+        {campus.name}
+        <Header.Subheader>
+          {campus.description}
+        </Header.Subheader>
+      </Header>
+      {/* call AllStudentsContainer with own list of students/campus */}
+      <AllStudentsContainer campus={campus}  />
+    </div>
+  );
+};
+
+export default SingleCampus;
